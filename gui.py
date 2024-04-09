61a17a2375de9e1ac6beacff5f450fe3bca2186b
Giovanni Cernuschi <s296831@studenti.polito.it>
2024-04-09 11:54:43 +0200

    h

--- gui.py
+++ gui.py
@@ -9,21 +9,22 @@ class sytdlGui:
     def __init__(self) -> None:
         self.root = tk.Tk()
         self.root.geometry("1000x900")
-        self.fonto = ('Arial, 18')
+        self.titleFont = ('Arial', 18)
+        self.Font=('Arial',16)
 
-        self.label = tk.Label(self.root, text="SYTDL", font=self.fonto)
+        self.label = tk.Label(self.root, text="SYTDL", font=self.titleFont)
         self.label.pack(padx=10,pady=10)
 
-        self.linkLabel = tk.Label(self.root, text="link")
+        self.linkLabel = tk.Label(self.root, text="link", font=self.Font)
         self.linkLabel.pack()
 
-        self.linktextbox = tk.Text(self.root, height=1, font=self.fonto)
+        self.linktextbox = tk.Text(self.root, height=1, font=self.Font)
         self.linktextbox.pack(padx=10,pady=10)
 
-        self.check = tk.Checkbutton(self.root, text="aaaaaa",font=self.fonto)
+        self.check = tk.Checkbutton(self.root, text="aaaaaa",font=self.Font)
         self.check.pack(padx=10,pady=10)
 
-        self.buttono = tk.Button(self.root,text="bbbbb",font=self.fonto,command=self.buttonFunc)
+        self.buttono = tk.Button(self.root,text="bbbbb",font=self.Font,command=self.buttonFunc)
         self.buttono.pack(padx=10,pady=10)
 
         self.root.mainloop() 
